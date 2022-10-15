40c00d7fcbb399aa1736604df8012e74de123a4b
LUL <108208140+LUUUUUUUUUUL@users.noreply.github.com>
2022-10-15 07:47:41 +0200

    Update BottomBar.js

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -16,7 +16,7 @@ export default function BottomBar(props) {
       >
         <Link to="/" className="navbar-brand">
           {" "}
-          <small>&copy; Copyright 2021, StagBIN</small>{" "}
+          <small>&copy; Copyright 2021, LUL</small>{" "}
         </Link>
       </nav>
     </div>
